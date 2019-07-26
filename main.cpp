a7955e14f91ff3ad0167ea9d891543335f761f54
Kremer Anna <sofiakremer7925@gmail.com>
2019-07-26 17:38:30 +0700

    chessviz

---
+++ main.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    cout << "8 r n b q k b n r" << endl
+         << "7 p p p p p p p p" << endl
+         << "6                " << endl
+         << "5                " << endl
+         << "4                " << endl
+         << "3                " << endl
+         << "2 P P P P P P P P" << endl
+         << "1 R N B Q K B N R" << endl
+         << "  a b c d e f g h" << endl;
+}
